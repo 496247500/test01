0e70821ced37124ad4067227393e87d0d57ceba0
majingchong <majingchong@wattman.cn>
2021-09-20 12:07:31 +0800

    增加一行输出

--- test.cc
+++ test.cc
@@ -1,4 +1,5 @@
 #include <iostream>
 int main(){
-    std::cout << "this is a test file" << std::endl;
+std::cout << "this is a test file" << std::endl;
+std::cout << "vim edit this file" <<std::endl;	
 }
