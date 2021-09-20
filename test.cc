4e90bc42eb415bc243de62890aecac53ea5222d7
majingchong <majingchong@wattman.cn>
2021-09-20 17:17:37 +0800

    调整缩进

--- test.cc
+++ test.cc
@@ -1,9 +1,9 @@
 #include <iostream>
 int main(){
-int b =20;
-std::cout << "b =" << b << std::endl;
-int a =10;
-std::cout << "a =" << a << std::endl;
-std::cout << "this is a test file" << std::endl;
-std::cout << "vim edit this file" <<std::endl;	
+    int b =20;
+    std::cout << "b =" << b << std::endl;
+    int a =10;
+    std::cout << "a =" << a << std::endl;
+    std::cout << "this is a test file" << std::endl;
+    std::cout << "vim edit this file" <<std::endl;	
 }
