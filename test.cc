f62887c95c14069ea4dd934492e62bd61047b7ef
majingchong <majingchong@wattman.cn>
2021-09-20 17:08:04 +0800

    changed the test.cc

--- test.cc
+++ test.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 int main(){
+int a =10;
+std::cout << "a =" << a << std::endl;
 std::cout << "this is a test file" << std::endl;
 std::cout << "vim edit this file" <<std::endl;	
 }
