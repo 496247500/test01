0f0bdf7e2eddbc18d0c2bd1e196b7b7ed8a63c60
majingchong <majingchong@wattman.cn>
2021-09-20 17:11:04 +0800

    修改代码

--- test.cc
+++ test.cc
@@ -1,5 +1,7 @@
 #include <iostream>
 int main(){
+int b =20;
+std::cout << "b =" << b << std::endl;
 std::cout << "this is a test file" << std::endl;
 std::cout << "vim edit this file" <<std::endl;	
 }
